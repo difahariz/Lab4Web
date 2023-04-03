68b15f250a7b8b3f5714c0e709e23deab705677a
Difa Hariz <difahariz@gmail.com>
2023-04-03 18:18:17 +0700

    filr ditambahkan

---
+++ lab4_php_modular/about.php
@@ -0,0 +1,8 @@
+<?php require('header.php'); ?>
+<div class="container">
+    <h2>About</h2>
+    <div class="box">
+        <p>Hai saya Difa Hariz Zhalifunnas mengucapkan terimakasih.</p>
+    </div>
+</div>
+<?php require('footer.php'); ?>
---
+++ lab4_php_modular/kontak.php
@@ -0,0 +1,13 @@
+<?php require('header.php'); ?>
+<div class="container">
+    <h2>Kontak</h2>
+    <div class="box">
+        <p>Nama : Difa Hariz Zhalifunnas</p>
+        <p>Nim : 312110572</p>
+        <p>Kelas : TI.21.A.1</p>
+        <p>Matkul : Pemograman Web 2</p>
+        <p>Email : difahariz@gmail.com</p>
+        <p>No. Telp : 089528804042</p>
+    </div>
+</div>
+<?php require('footer.php'); ?>
